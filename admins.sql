92f28a8ae1b0a486310473cba27e6e156a1f5ff4
Nimanthadilshan12 <145957644+Nimanthadilshan12@users.noreply.github.com>
2025-06-15 11:02:04 +0530

    Add files via upload

---
+++ admins.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS admins (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        UserID VARCHAR(50) UNIQUE NOT NULL,
+        Password VARCHAR(255) NOT NULL,
+        Fullname VARCHAR(100) NOT NULL,
+        Email VARCHAR(100) NOT NULL,
+        Contact_No VARCHAR(20) NOT NULL
+    );
