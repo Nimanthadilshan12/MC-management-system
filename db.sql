d65eaab064b6c86b061e1b14927bdb7a2f57c0dd
Nimanthadilshan12 <145957644+Nimanthadilshan12@users.noreply.github.com>
2025-06-15 11:00:48 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,10 +1,1 @@
-CREATE DATABASE university_medical;
 
-USE university_medical;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('Patient', 'Doctor', 'Admin', 'Pharmacist') NOT NULL
-);
