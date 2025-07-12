ae2e7a0ecbc3119ed786f80f83c744045db2f6de
Maleesha010512 <maleesha20010512@gmail.com>
2025-07-12 12:00:01 +0530

    Delete Create_new_bill8.php

--- Create_new_bill8.php
+++
@@ -1,6 +0,0 @@
-$patientID = $_POST['PatientID'];
-$amount = $_POST['Amount'];
-
-$stmt = $conn->prepare("INSERT INTO billing (PatientID, Amount, DateIssued) VALUES (?, ?, CURDATE())");
-$stmt->bind_param("id", $patientID, $amount);
-$stmt->execute();
