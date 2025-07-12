3ce709acc1b63abba80426dddbea23a1fcd160c9
Maleesha010512 <maleesha20010512@gmail.com>
2025-07-12 12:03:00 +0530

    Delete Update_inventory_on_dispensing4.php

--- Update_inventory_on_dispensing4.php
+++
@@ -1,6 +0,0 @@
-$medicineID = $_POST['MedicineID'];
-$dispenseQty = $_POST['Quantity'];
-
-$stmt = $conn->prepare("UPDATE inventory SET Quantity = Quantity - ? WHERE MedicineID = ?");
-$stmt->bind_param("ii", $dispenseQty, $medicineID);
-$stmt->execute();
