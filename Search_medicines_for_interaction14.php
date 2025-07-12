da89f51ae047c3d9096d65f08a44222308ce4938
Maleesha010512 <maleesha20010512@gmail.com>
2025-07-12 12:05:58 +0530

    Delete Search_medicines_for_interaction14.php

--- Search_medicines_for_interaction14.php
+++
@@ -1,6 +0,0 @@
-$term = $_GET['term'];
-$query = "SELECT * FROM drug_info WHERE DrugName LIKE '%$term%'";
-$result = $conn->query($query);
-while ($row = $result->fetch_assoc()) {
-    echo $row['DrugName'] . "<br>";
-}
