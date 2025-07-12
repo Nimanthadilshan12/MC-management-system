8eae727e220230214a67f95dc53666c8229027e1
Maleesha010512 <maleesha20010512@gmail.com>
2025-07-12 12:02:02 +0530

    Delete Fetch_pending_prescriptions2.php

--- Fetch_pending_prescriptions2.php
+++
@@ -1,5 +0,0 @@
-$query = "SELECT * FROM prescriptions WHERE Status = 'Pending'";
-$result = $conn->query($query);
-while ($row = $result->fetch_assoc()) {
-    echo "Medication: " . $row['Medication'] . "<br>";
-}
