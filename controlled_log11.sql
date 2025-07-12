48304f6167946b5548fc5d59086684a89a274a51
Maleesha010512 <maleesha20010512@gmail.com>
2025-07-12 12:05:07 +0530

    Delete controlled_log11.sql

--- controlled_log11.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE controlled_log (
-    LogID INT AUTO_INCREMENT PRIMARY KEY,
-    MedicineName VARCHAR(255),
-    Quantity INT,
-    Action ENUM('Received', 'Dispensed'),
-    DateLogged DATE
-);
